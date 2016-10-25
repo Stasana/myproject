313e487560c8c3b0b918f89d09882f14b1ce264c
VikaMi <viktoria.mishchanyn@gmail.com>
2016-10-26 00:09:43 +0300

    secondversion

--- vingener.c
+++ vingener.c
@@ -1,7 +1,8 @@
-#include <cs50.h> 
+﻿#include <cs50.h> 
 #include <stdio.h> 
 #include <ctype.h> 
 #include <string.h> 
+//зчитування рядка тексту
 int main(int argc, string argv[]) 
 { 
 if (argc != 2) 
